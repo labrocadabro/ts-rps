dea5599edeafce0e832cdd01cc6d1479f497172b
Laura <moomal.abro@gmail.com>
2022-10-30 08:23:51 +0500

    complete functionality

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
+var winner = {
+    "rock": "scissors",
+    "scissors": "paper",
+    "paper": "rock"
+};
 var hands = document.querySelectorAll('.hand');
 hands.forEach(function (hand) { return hand.addEventListener('click', playerChoose); });
 function playerChoose(e) {
-    var choice = this.cloneNode(true);
-    document.getElementById('result').append(choice);
+    var playerChoice = this.cloneNode(true);
+    var player1 = document.getElementById('player1');
+    player1.innerHTML = "<h2>Your Choice</h2>";
+    player1.append(playerChoice);
+    var computerChoice = hands[Math.floor(Math.random() * 3)];
+    var compChoiceClone = computerChoice.cloneNode(true);
+    var player2 = document.getElementById('player2');
+    player2.innerHTML = "<h2>My Choice</h2>";
+    player2.append(compChoiceClone);
+    hands.forEach(function (hand) { return hand.removeEventListener('click', playerChoose); });
+    var player1Result = playerChoice.id;
+    var player2Result = computerChoice.id;
+    var showWinner = document.getElementById('winner');
+    if (winner[player1Result] === player2Result) {
+        showWinner.innerHTML = "<h1>You win!</h1>";
+    }
+    else if (winner[player2Result] === player1Result) {
+        showWinner.innerHTML = "<h1>I win!</h1>";
+    }
+    else {
+        showWinner.innerHTML = "<h1>It's a draw!</h1>";
+    }
+    var again = document.createElement('button');
+    again.id = "again";
+    again.innerText = "Play Again";
+    again.addEventListener('click', function () { return location.reload(); });
+    showWinner.append(again);
 }
--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
+const winner = {
+	"rock": "scissors",
+	"scissors": "paper",
+	"paper": "rock"
+}
+
 const hands = document.querySelectorAll('.hand') as NodeListOf<HTMLDivElement>;
 
 hands.forEach(hand => hand.addEventListener('click', playerChoose));
 
 function playerChoose(e: Event) {
-	const choice = this.cloneNode(true);
-	document.getElementById('result')!.append(choice);
+	const playerChoice = this.cloneNode(true);
+	const player1 = document.getElementById('player1')!;
+	player1.innerHTML = "<h2>Your Choice</h2>";
+	player1.append(playerChoice);
+	const computerChoice = hands[Math.floor(Math.random() * 3)];
+	const compChoiceClone = computerChoice.cloneNode(true);
+	const player2 = document.getElementById('player2')!;
+	player2.innerHTML = "<h2>My Choice</h2>";
+	player2.append(compChoiceClone);
+	hands.forEach(hand => hand.removeEventListener('click', playerChoose));
+	const player1Result = playerChoice.id;
+	const player2Result = computerChoice.id;
+	const showWinner = document.getElementById('winner')!;
+	if (winner[player1Result] === player2Result) {
+		showWinner.innerHTML = "<h1>You win!</h1>";
+	} else if (winner[player2Result] === player1Result) {
+		showWinner.innerHTML = "<h1>I win!</h1>";
+	} else {
+		showWinner.innerHTML = "<h1>It's a draw!</h1>";
+	}
+	const again = document.createElement('button');
+	again.id = "again";
+	again.innerText = "Play Again";
+	again.addEventListener('click', () => location.reload());
+	showWinner.append(again);
 }
